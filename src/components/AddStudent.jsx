ef9616caea5b55d99841d38739914628ffee8c1e
shaniba17 <shanibatm2002@gmail.com>
2024-06-09 22:31:52 +0530

    project completed

--- src/components/AddStudent.jsx
+++ src/components/AddStudent.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from './NavBar'
 
 const AddStudent = () => {
-  return (
+    const [data, setData] = useState(
+        {
+            
+            "_id": "",
+            "firstname": "",
+            "lastname": "",
+            "college": "",
+            "dob": "",
+            "course": "",
+            "mobile": "",
+            "email": "",
+            "address": "",
+            "__v": ""
+        }
+    )
+    const inputHandler=(event)=>
+        {
+           setData({...data,[event.target.name]:event.target.value})
+        }
+    const readValue=()=>
+        {
+            console.log(data)
+        }
+return (
     <div>
-        <NavBar/>
+        <NavBar />
         <div className="container">
             <div className="row">
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
@@ -14,29 +37,29 @@ const AddStudent = () => {
                         </u>
                     </h1>
                     <div className="row g-3">
-                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">Id</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='_id' value={data._id} onChange={inputHandler}/>
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">First Name</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='firstname' value={data.firstname} onChange={inputHandler}/>
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">Last Name</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='lastname' value={data.lastname} onChange={inputHandler} />
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">College</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='college' value={data.college} onChange={inputHandler} />
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">Date of Birth</label>
-                            <input type="date" name="" id="" className="form-control" />
+                            <input type="date" name="dob" id="" className="form-control" value={data.dob} onChange={inputHandler}/>
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">Course</label>
-                            <select name="" id="" className="form-control">
+                            <select name="course" id="" className="form-control" value={data.course} onChange={inputHandler}>
                                 <option value="MCA">MCA</option>
                                 <option value="MBA">MBA</option>
                                 <option value="BTECH">BTECH</option>
@@ -45,25 +68,25 @@ const AddStudent = () => {
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">Mobile No</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='mobile' value={data.mobile} onChange={inputHandler}/>
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">Email Id</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='email' value={data.email} onChange={inputHandler}/>
                         </div>
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                             <label htmlFor="" className="form-label">Address</label>
-                            <textarea name="" id="" className="form-control"></textarea>
+                            <textarea name="address" id="" className="form-control" value={data.address} onChange={inputHandler}></textarea>
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <button className="btn btn-success">Submit</button>
+                            <button className="btn btn-success" onClick={readValue}>Submit</button>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-  )
+)
 }
 
 export default AddStudent
--- src/components/ViewStudent.jsx
+++ src/components/ViewStudent.jsx
@@ -1,34 +1,21 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import NavBar from './NavBar'
+import axios from 'axios'
 
 const ViewStudent = () => {
-    const [data,changedata]=useState([
+    const [data,changedata]=useState([])
+    const fetchData=()=>
         {
-            "_id": "66651683741a512717d92b87",
-            "firstname": "Manu",
-            "lastname": "R",
-            "college": "FISAT",
-            "dob": "02/04/1999",
-            "course": "B-Tech Comp Science",
-            "mobile": "+91 95266 7443",
-            "email": "aa@gmail.com",
-            "address": "Kochi",
-            "__v": 0
-            },
-            {
-            "_id": "666516bc741a512717d92b88",
-            "firstname": "Rahul",
-            "lastname": "D",
-            "college": "FISAT",
-            "dob": "02/01/1992",
-            "course": "MCA",
-            "mobile": "+91 95266 74440",
-            "email": "aa@gmail.com",
-            "address": "Test Address",
-            "__v": 0
-            }
-
-    ])
+            axios.get("https://anishpdm.github.io/dummy-api-new/student.json")
+            .then(
+                response =>{
+                    console.log(response.data)
+                    changedata(response.data)
+                }
+            )
+            .catch().finally()
+        }
+        useEffect(()=>{fetchData()},[])
     return (
         <div>
             <NavBar/>
